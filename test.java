2578d1281d371774d96120ef0b414fb65ab08e82
unknown <hrittik97@gmail.com>
2024-11-21 10:39:53 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        // int a=10;
+        // int b=20;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1");
+        int a=sc.nextInt();
+        System.out.println("enter 2");
+        int b=sc.nextInt();
+
+        int sum=a+b;
+
+        System.out.println(sum);
+    }
+}
